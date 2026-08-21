cd2b4b52ff7f6c80ced3b6a9fdbdd6d1aa03892e
Marcella... <rodrimarcella2024@gmail.com>
2026-08-21 10:20:38 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -147,8 +147,7 @@ app.get("/medicacoes", (req, res) => {
 
 // START
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
 
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Servidor rodando na porta ${PORT}`);
-
 });
